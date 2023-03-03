1a3cd4076d435156a5b8eae97de3f339383ecb00
Salucatelli <119860566+Salucatelli@users.noreply.github.com>
2023-03-03 16:33:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+// EX 1)
+
+
+
+// let indice = 13;
+// let k = 0;
+// let soma = 0;
+
+// while (k < indice) {
+//     k += 1;
+//     soma += k;
+// }
+
+// console.log(soma);
+
+
+
+//EX 2)
+
+// let num = 1;
+
+// console.log("Vamos verificar se o número " + num + " está na sequência de Fibonacci: ");
+// console.log("Executando sequência de Fibonacci...")
+
+// let x1 = 0;
+// let x2 = 1;
+// let x = 0;
+// let c = 0;
+
+// while (0 == 0) {
+//     if (c == 0) {
+//         x = 0;
+//         console.log(0);
+//         console.log(1);
+//     }
+//     if (num == x) {
+//         console.log("O número " + num + " está presente na sequência de Fibonacci.");
+//         break
+//     } else if (x > num) {
+//         console.log("O número " + num + " não está presente na sequência de Fibonacci.");
+//         break
+//     }
+//     x = x1 + x2;
+//     console.log(x)
+//     x1 = x2;
+//     x2 = x;
+//     c++
+
+// }
+
+
+
+//EX 5)
+
+
+// function inverter(string) {
+//     var saida = "";
+//     for (var i = string.length - 1; i >= 0; i--) {
+
+//         saida += string[i];
+
+//     }
+//     console.log(saida)
+// }
+
+// inverter("target");
